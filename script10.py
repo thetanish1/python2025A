fa61ae91f5f5940369a2b5640df12e8fcab013c8
unknown <Minskoleopc>
2025-03-03 21:59:49 +0530

    added

---
+++ script10.py
@@ -0,0 +1,115 @@
+
+# variable 
+# int , float , boolean str
+# type --- comparison , logical operator
+# conditional statement , loops for while 
+# list - CRUD  -- create , retrive , update ,delete / loop - methods
+# dict - CRUD   --  loop - methods
+# tuple - Cr - methods  CRUD
+# str - CRUD , Methods
+
+fn =  "chinmay"
+e = fn.upper()
+print(e)
+
+ln = "Deshpande"
+e2 = ln.lower()
+print(e2)
+
+mn = "chinmay"
+e3 = mn.capitalize()
+print(e3)
+
+title = "I am learning js"
+e4 = title.title()
+print(e4)
+
+
+# String format methds 
+f = " goa "
+print(len(f))
+e5 = f.strip()
+print(e5)
+print(len(e5))
+
+f2 = " goa "
+print(len(f2))
+e6 = f2.lstrip()
+print(e6)
+
+f3 = "goa "
+print(len(f3))
+e7 = f3.rstrip()
+print(len(e7))
+
+s = "hello"
+e8 = s.center(10,'-')
+print(e8)
+
+#  --hello---
+
+s = "hello"
+e9 = s.ljust(10,'-')
+print(e9)
+
+s = "hello"
+e10 = s.rjust(10,'-')
+print(e10) 
+
+#zfill
+ide = '89'
+e = ide.zfill(5)
+print(e)
+
+# String check methods 
+
+print('abc'.isalpha())
+print('123'.isnumeric())
+print('123'.isalnum())
+print('123aaa'.isalnum())
+print('aaa'.isalnum())
+print(''.isalnum())
+print('abc'.islower())
+print('ABC'.isupper())
+print(" ".isspace())
+print("I Am Learning Js".istitle())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
