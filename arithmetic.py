f31626c169972778f5e0fce99411cd46039c3f9a
gitminskole <git.minskole@gmail.com>
2025-04-01 21:53:55 +0530

    added

---
+++ arithmetic.py
@@ -0,0 +1,12 @@
+# module file
+def add(x,y):
+    print(x+y)
+
+def sub(x,y):
+    print(x-y)
+
+def mul(x,y):
+    print(x*y)
+
+def div(x,y):
+    print(x/y)
---
+++ use.py
@@ -0,0 +1,23 @@
+# import  arithmetic as ar
+# ar.add(12,4)
+# ar.sub(12,5)
+# ar.mul(12,5)
+# ar.div(15,5)
+
+# from arithmetic import add,sub
+# add(12,3)
+# sub(12,3)
+
+# from arithmetic import *
+# add(12,3)
+# sub(12,4)
+# div(12,5)
+# mul(12,6)
+
+# import arithmetic
+# arithmetic.add(12,4)
+# arithmetic.sub(12,4)
+# arithmetic.mul(12,4)
+# arithmetic.div(12,4)
+
+# function , class , const
