a8310f9f5242950972439d5dbed224f706cdbfc2
gitminskole <git.minskole@gmail.com>
2025-06-12 08:19:18 +0530

    added

---
+++ 800AM/pythonOne.py
@@ -0,0 +1,32 @@
+x = 10
+print(x)
+x = 100
+print(x)
+
+# Arithmetic operation 
+a = 10 
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+s = 9
+t = 3
+
+print(s+t)
+print(s-t)
+print(s*t)
+print(s/t)
+print(s%t)
+
+def Calculator(x,y):
+    print(x+y)
+    print(x-y)
+    print(x*y)
+    print(x/y)
+    print(x%y)
+Calculator(6,3)
+Calculator(15,5)
