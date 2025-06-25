4875ead9508e6b35ea81635a2c6e5f76f132d258
gitminskole <git.minskole@gmail.com>
2025-06-25 17:52:43 +0530

    added

---
+++ 800AM/dayNine.py
@@ -0,0 +1,110 @@
+# #9:30  pm
+# # Excel ----->monika,mansi,vrushali 
+
+# # name = "chinmay"
+# # print(name)
+# # print(type(name))
+
+# # lastName = 'deshpande'
+# # print(lastName)
+# # print(type(lastName))
+
+# # middleName = """
+# #     chinmay
+# # """
+# # print(middleName)
+
+# # middleName = '''
+# #     sarika
+# # '''
+# # print(middleName)
+
+# # program 1
+# # 0   1   2   3
+# # p   u   n   e
+# city = "pune"
+# print(city[1])
+# print(city[-1])
+
+# # program 2
+# names = ["chinmay","sarika","poorva"]
+# names[0] = "mansi"
+# print(names)
+
+# # cannot update one single element
+# # city[0] = "P"
+# city = "Pune"
+
+# # program 4
+# country = "india"
+# e = len(country)
+# print(e)
+
+# #program  5
+# fruits = "apple mango banana organe grapes"
+# print("pple " in fruits)
+
+# #program 6
+# # loop 
+# # range()
+
+# # 0    1     2     3   4   5
+# # n    a     g     p   u   r  
+# # -6  -5    -6    -3  -2   -1
+
+# city = 'nagpur'
+# # for x in range(len(city)):
+# #     #print(x)
+# #     print(city[x])
+
+
+# for x in range(len(city)-1,-1,-1):
+#     #print(x)
+#     print(city[x])
+
+# city  = "goa"
+# # 0    1    2
+# # g    o    a
+# print(len(city) - 1)
+
+
+# #program 7
+# firstName = "chinmay"
+# for char in firstName:
+#     print(char)
+
+# #program 8
+# # 0   1   2   3  4
+# # m   a   n   g  o
+
+# fruitOne  = "mango"
+# j = 0
+# while(j < 5):
+#     print(fruitOne[j])
+#     j = j + 1
+
+# j1 = len(fruitOne) - 1
+# while(j1 >= 0):
+#     print(fruitOne[j1])
+#     j1 = j1 - 1
+
+
+# # methods 
+
+# city = "Pune"
+# e = city.upper()
+# print(e)
+
+# e2 = city.lower()
+# print(e2)
+# python ----> advanced 
+# js     ----> complete
+# excel  ----> not completed
+# sql    ----> completed 
+# power BI ----> not completed
+# postman   ----> not completed
+# git       ----> not completed
+
+
+
+
