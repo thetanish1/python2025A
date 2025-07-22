4543c91fed5ad2916e07ac9064f27e1b83872419
gitminskole <git.minskole@gmail.com>
2025-07-22 08:26:30 +0530

    added

---
+++ 800AM/day19.py
@@ -0,0 +1,43 @@
+
+# class  Person:
+#     fn = None
+#     ln = None
+
+#     def displayName(self):
+#         print(self.fn + self.ln)
+
+
+# chinmay  = Person()
+# print(chinmay.fn)
+# print(chinmay.ln)
+# chinmay.fn  = "chinmay"
+# chinmay.ln = "deshpande"
+# chinmay.displayName()
+
+# amol = Person()
+# print(amol.fn)
+# print(amol.ln)
+# #amol.displayName()
+# amol.fn = 'amol'
+# amol.ln = 'rao'
+# amol.displayName()
+# print(amol.fn)
+# print(amol.ln)
+
+# Program 2
+# class Person:
+#     def __init__(self,fn,ln):
+#         self.firstName = fn
+#         self.lastName = ln 
+
+#     def displayName(self):
+#         print(self.firstName + self.lastName)
+
+# sarika = Person("sarika","pansare")
+# sarika.displayName()
+
+# amol = Person("amol","rao")
+# amol.displayName()
+
+# 8.00 am
+# 8:15 am
