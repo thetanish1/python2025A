94c12c3c8dc8ef8cdbcfed093aebeab7ea19f449
thetanish1 <tanishdewase25@gmail.com>
2025-08-12 23:02:03 +0530

    [python] immutable,referance variable

---
+++ daythree.py
@@ -0,0 +1,138 @@
+# vehicles={
+#     "color":"blue",
+#     "type":"sedan",
+#     "companyname":"audi"
+# }
+
+# print(vehicles)
+# print(type(vehicles))
+
+# print(vehicles.get('color'))
+
+# for x in vehicles:
+#     print(x,vehicles[x])
+
+# vehicles.update({"city":"pune"}) #update the dict
+# print(vehicles)
+
+
+# e=vehicles.setdefault('city','nagpur')
+# print(e)
+
+# vehicles.setdefault('wheel','4')
+# print(vehicles)
+
+
+# listA=[11,22,33,44]
+# print(type(listA[1]))
+
+# listB=['tanish','disha','rishika','ishika']
+# print(type(listB[1]))
+
+# maxvalue=max(listA)
+# print(maxvalue)
+
+# minvalue=min(listA)
+# print(minvalue)
+
+# for x in range(len(listB)):
+#     print(listB[x])
+
+# for i in range(len(listA)):
+#     print(listA[i])
+
+# continue statements
+
+# for x in range(1,10):
+#     if x==4:
+#         break
+#     print(x)
+
+# for x in range(1,11):
+#     if x==5:
+#         continue
+#     print(x)
+
+# i1=1
+# while(i1<11):
+#     print(i1)
+#     i1+=1
+
+# i1=100
+# while(i1>0):
+#     print(i1)
+#     i1-=1
+
+# i1=1
+# while(i1<11):
+#     print(i1*2)
+#     i1+=1
+
+#table of 5 in reverse
+
+# i1=10
+# while(i1>0):
+#     print(i1*5)
+#     i1-=1
+
+#methods
+# x=100
+# y=x
+# y=600
+# print(x)
+# print(y)
+
+# a = [1, 2, 3]  #list is immutable ???
+# b = a
+# b.append(4)
+# print(a)
+# print(b)
+
+# p = 10
+# q = p
+# q += 5
+# print(p)
+# print(q)
+
+
+# x = [10, 20]
+# y = x
+# y = [30, 40]
+# print(x)
+# print(y)
+
+# nums1 = [1, 2]
+# nums2 = nums1
+# nums2[0] = 100
+# print(nums1)
+# print(nums2)
+
+# m = "hello"
+# n = m
+# n = n.upper()
+# print(m)
+# print(n)
+
+# list1 = [1, 2, 3]
+# list2 = list1.copy()
+# list2.append(4)
+# print(list1)
+# print(list2)
+
+# t1 = (1, 2, 3)
+# t2 = t1
+# t2 = (4, 5, 6)
+# print(t1)
+# print(t2)
+
+# data = {"a": 1}
+# alias = data
+# alias["b"] = 2
+# print(data)
+# print(alias)
+
+
+
+
+
+
